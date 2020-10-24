aa01ec02daf184c6036f3654c9720a158b3ef575
ShravyaHubb <shravyak146@gmail.com>
2020-10-24 19:12:16 +0530

    Added file: SortByName.java to sort address book alphabetically

---
+++ SortByName.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class SortByName implements Comparator<Person> {
+
+    @Override
+    public int compare(Person person1, Person person2) {
+
+        return (person1.firstName).compareTo(person2.firstName);
+
+    }
+
+}
+
