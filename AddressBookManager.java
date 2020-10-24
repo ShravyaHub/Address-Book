70f51d4581c4e531e5fb48b3f797ed35b437fffa
ShravyaHubb <shravyak146@gmail.com>
2020-10-24 17:03:49 +0530

    Modified file: AddressBookManager.java, added to code to add a new person to the address book

--- AddressBookManager.java
+++ AddressBookManager.java
@@ -1,17 +1,29 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBookManager implements IAddressBook{
 
     @Override
     public void createPerson() {
-        ArrayList<String> contacts = new ArrayList<>();
-        contacts.add("Shravya");
-        contacts.add("K");
-        contacts.add("Bangalore");
-        contacts.add("Bangalore");
-        contacts.add("Karnataka");
-        contacts.add("560062");
-        contacts.add("9591266654");
+        ArrayList<Person> contacts = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter last name: ");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter address : ");
+        String address = scanner.nextLine();
+        System.out.println("Enter city: ");
+        String city = scanner.nextLine();
+        System.out.println("Enter state: ");
+        String state = scanner.nextLine();
+        System.out.println("Enter zip: ");
+        String zip = scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        String phoneNumber = scanner.nextLine();
+        Person person = new Person(firstName, lastName, address, city, state, zip, phoneNumber);
+        contacts.add(person);
+        System.out.println("Contact added: " + person.firstName + " " + person.lastName);
     }
 
 }
